6dcbd2cce80653cbecdbbbcb7ea8b6c6eb8df619
MiniNinja102r <vadimmuxametdinov5@gmail.com>
2025-10-12 03:04:53 +0300

    Вынесены данные в конфигурационный файл

---
+++ src/main/java/com/example/infinity/storage/Config.java
@@ -0,0 +1,53 @@
+package com.example.infinity.storage;
+
+import lombok.extern.java.Log;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Map;
+
+@Log
+public final class Config {
+    private static Map<String, Object> data;
+
+    // Подавление создания стандартного конструктора.
+    private Config() {}
+
+    public static void load() {
+        try (InputStream input = Config.class.getResourceAsStream("/com/example/infinity/config.yml")) {
+            if (input == null) {
+                log.severe("Config file not found");
+                return;
+            }
+            final Yaml yaml = new Yaml();
+            data = yaml.load(input);
+            loadResources();
+        } catch (IOException e) {
+            log.severe("Error reading resource configuration: " + e);
+        }
+    }
+
+    private static void loadResources() {
+        Map<String, Object> resource = (Map<String, Object>) data.get("resource");
+        if (resource == null)
+            log.warning("Resource section not found in config");
+        else {
+            Resource.PATH = (String) resource.get("path");
+            Resource.FXML_PATH = (String) resource.get("fxml");
+            Resource.IMG_PATH = (String) resource.get("image");
+            Resource.AUDIO_PATH = (String) resource.get("audio");
+        }
+    }
+
+    public static class Resource {
+
+        // Подавление создания стандартного конструктора.
+        private Resource() {}
+
+        public static String PATH;
+        public static String FXML_PATH;
+        public static String IMG_PATH;
+        public static String AUDIO_PATH;
+    }
+}
